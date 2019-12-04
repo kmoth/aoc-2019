5bc5d1d6df27af424bd600e3dc17049c10704c63
Kelsey Mott <kelseymott@gmail.com>
2019-12-04 15:52:46 -0500

    day 2 done

--- IntcodeComputer.cs
+++ IntcodeComputer.cs
@@ -1,70 +1,99 @@
 using System;
+using System.Collections.Generic;
 
 namespace aoc
 {
     class IntcodeComputer
     {
-        private int[] inputs = new int[]{
-            1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,9,23,1,23,6,27,1,9,27,31,1,31,10,35,2,13,35,39,1,39,10,43,1,43,9,47,1,47,13,51,1,51,13,55,2,55,6,59,1,59,5,63,2,10,63,67,1,67,9,71,1,71,13,75,1,6,75,79,1,10,79,83,2,9,83,87,1,87,5,91,2,91,9,95,1,6,95,99,1,99,5,103,2,103,10,107,1,107,6,111,2,9,111,115,2,9,115,119,2,13,119,123,1,123,9,127,1,5,127,131,1,131,2,135,1,135,6,0,99,2,0,14,0};
+        private int[] liveInputs = new int[]{1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,9,23,1,23,6,27,1,9,27,31,1,31,10,35,2,13,35,39,1,39,10,43,1,43,9,47,1,47,13,51,1,51,13,55,2,55,6,59,1,59,5,63,2,10,63,67,1,67,9,71,1,71,13,75,1,6,75,79,1,10,79,83,2,9,83,87,1,87,5,91,2,91,9,95,1,6,95,99,1,99,5,103,2,103,10,107,1,107,6,111,2,9,111,115,2,9,115,119,2,13,119,123,1,123,9,127,1,5,127,131,1,131,2,135,1,135,6,0,99,2,0,14,0};
 
-        public void Run()
+        private int[] memory;
+
+        private Dictionary<int, Opcode> opcodes = new Dictionary<int, Opcode>();
+
+        private delegate int Opcode(params int[] values);
+
+        public IntcodeComputer()
         {
-            Console.WriteLine($"Total: {inputs.Length}, Opcodes: {(inputs.Length / 4f)}");
-            RestoreAlarmState();
-            
-            for (int i = 0; i < inputs.Length; i += 4)
+            InitOpcodesLookup();
+        }
+
+        public int Run(int noun, int verb)
+        {
+            Reset();
+            SetInputs(noun, verb);
+            ProcessInputs();
+
+            return memory[0];
+        }
+
+        private void Reset()
+        {
+            memory = new int[liveInputs.Length];
+            Array.Copy(liveInputs, memory, liveInputs.Length);
+        }
+
+        private void InitOpcodesLookup()
+        {
+            opcodes.Add(1, Add);
+            opcodes.Add(2, Multiply);
+            opcodes.Add(99, Exit);
+        }
+
+        private void ProcessInputs()
+        {
+            int lengthMinusOne = memory.Length - 1;
+
+            for (int i = 0; i < memory.Length; i += 4)
             {
-                if (ProcessOpcode(i))
+                int opcodeIndex = memory[i];
+                int value1Index = memory[Math.Min(i + 1, lengthMinusOne)];
+                int value2Index = memory[Math.Min(i + 2, lengthMinusOne)];
+                int destinationIndex = memory[Math.Min(i + 3, lengthMinusOne)];
+
+                int resultCode = opcodes[opcodeIndex].Invoke(
+                    value1Index, 
+                    value2Index, 
+                    destinationIndex);
+
+                if (resultCode > 0)
                 {
                     break;
                 }
             }
         }
 
-        private void RestoreAlarmState()
+        private void SetInputs(int noun, int verb)
         {
-            inputs[1] = 12;
-            inputs[2] = 2;
+            memory[1] = noun;
+            memory[2] = verb;
         }
 
-        private bool ProcessOpcode(int start)
+        private int Add(params int[] values)
         {
-            switch(inputs[start])
-            {
-                case 1:
-                    DoAdd(start);
-                    return false;
+            int value1 = values[0];
+            int value2 = values[1];
+            int destinationIndex = values[2];
 
-                case 2:
-                    DoMultiply(start);
-                    return false;
+            memory[destinationIndex] = memory[value1] + memory[value2];
 
-                case 99:
-                    DoComplete();
-                    return true;
-
-                default:
-                    return true;
-            }
+            return 0;
         }
 
-        private void DoAdd(int start)
+        private int Multiply(params int[] values)
         {
-            // Console.WriteLine($"ADD: {start}");
+            int value1 = values[0];
+            int value2 = values[1];
+            int destinationIndex = values[2];
 
-            inputs[start + 3] = inputs[start + 1] + inputs[start + 2];
+            memory[destinationIndex] = memory[value1] * memory[value2];
+
+            return 0;
         }
 
-        private void DoMultiply(int start)
+        private int Exit(params int[] values)
         {
-            // Console.WriteLine($"MUL: {start}");
-
-            inputs[start + 3] = inputs[start + 1] * inputs[start + 2];
-        }
-
-        private void DoComplete()
-        {
-            Console.WriteLine($"COMPLETE: {inputs[0]}");
+            return 1;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,8 +13,32 @@ namespace aoc
 
             // DAY 2
             IntcodeComputer computer = new IntcodeComputer();
-            computer.Run();
-            //
+            // computer.Run(12, 2);
+
+            int targetResult = 19690720;
+            
+            int result = IntcodeRunner(computer, targetResult);
+            Console.WriteLine($"Result={result}");
+        }
+
+        private static int IntcodeRunner(IntcodeComputer computer, int targetResult)
+        {
+            int x, y = 0;
+
+            for (x = 0; x < 100; x++)
+            {
+                for (y = 0; y < 100; y++)
+                {
+                    int result = computer.Run(x, y);
+
+                    if (result == targetResult)
+                    {
+                        return 100 * x + y;
+                    }
+                }
+            }
+
+            return -1;
         }
     }
 }
